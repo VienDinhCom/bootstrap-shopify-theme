48ec2bd0d9379af13939b26b7cb196c301e430e9
Vien Dinh <maxvien@gmail.com>
2021-04-22 18:03:16 +0700

    refactor with parceljs

--- gulpfile.js
+++ gulpfile.js
@@ -2,161 +2,17 @@ const fs = require('fs');
 const gulp = require('gulp');
 const yaml = require('yaml');
 const path = require('path');
-const cssnano = require('cssnano');
-const merge = require('merge-stream');
 const browserSync = require('browser-sync');
-const autoprefixer = require('autoprefixer');
 const themeKit = require('@shopify/themekit');
 const plugins = require('gulp-load-plugins')();
 
-const isDev = process.env.NODE_ENV === 'development';
+const Bundler = require('parcel-bundler');
+
+const entryFile = path.join(__dirname, 'src/main.js');
+const outDir = path.join(__dirname, 'dist/assets');
+
+const isProd = process.env.NODE_ENV === 'production';
 const config = yaml.parse(fs.readFileSync('config.yml', 'utf-8')).development;
-
-function parse(type) {
-  return plugins.tap((file) => {
-    let contents = file.contents?.toString() || '';
-
-    function getTag(tag) {
-      const tagStart = contents.indexOf(`<${tag} code>`);
-      const hasTag = tagStart >= 0;
-
-      if (!hasTag) return { tag: '', content: '' };
-
-      const contentStart = tagStart + tag.length + 7;
-      const contentEnd = contents.indexOf(`</${tag}>`, contentStart);
-      const tagEnd = contentEnd + tag.length + 3;
-
-      return {
-        tag: contents.substring(tagStart, tagEnd),
-        content: contents.substring(contentStart, contentEnd).trim(),
-      };
-    }
-
-    const style = getTag('style');
-    const script = getTag('script');
-
-    switch (type) {
-      case 'template':
-        contents = contents.replace(style.tag, '');
-        contents = contents.replace(script.tag, '');
-        break;
-
-      case 'style':
-        contents = style.content;
-        break;
-
-      case 'script':
-        contents = style.content.length ? `(() => {${script.content}})();` : '';
-        break;
-
-      default:
-        throw new Error('Could not parse!');
-    }
-
-    file.contents = Buffer.from(contents.trim(), 'utf-8');
-  });
-}
-
-const sources = [
-  'src/layout/*.liquid',
-  'src/snippets/*.liquid',
-  'src/sections/*.liquid',
-  'src/templates/**/*.liquid',
-];
-
-gulp.task('templates', () => {
-  return gulp
-    .src(sources)
-    .pipe(parse('template'))
-    .pipe(
-      plugins.if(
-        ({ path }) => path.indexOf('/src/layout/') > 0,
-        gulp.dest('dist/layout'),
-        plugins.if(
-          ({ path }) => path.indexOf('/src/snippets/') > 0,
-          gulp.dest('dist/snippets'),
-          plugins.if(
-            ({ path }) => path.indexOf('/src/sections/') > 0,
-            gulp.dest('dist/sections'),
-            plugins.if(
-              ({ path }) => path.indexOf('/src/templates/') > 0,
-              gulp.dest('dist/templates')
-            )
-          )
-        )
-      )
-    );
-});
-
-gulp.task('styles', () => {
-  return gulp
-    .src(['src/global/global.scss', ...sources])
-    .pipe(plugins.if((file) => path.extname(file.path) === '.liquid', parse('style')))
-    .pipe(plugins.concat('main.scss'))
-    .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(
-      plugins.if(
-        () => isDev,
-        plugins.postcss([autoprefixer()]),
-        plugins.postcss([autoprefixer(), cssnano()])
-      )
-    )
-    .pipe(plugins.if(() => isDev, plugins.prettier()))
-    .pipe(gulp.dest('dist/assets'));
-});
-
-gulp.task('scripts', () => {
-  return gulp
-    .src(['src/global/global.js', ...sources])
-    .pipe(plugins.if((file) => path.extname(file.path) === '.liquid', parse('script')))
-    .pipe(plugins.concat('main.js'))
-    .pipe(plugins.babel({ presets: ['@babel/env'] }))
-    .pipe(plugins.if(() => isDev, plugins.prettier(), plugins.uglify()))
-    .pipe(gulp.dest('dist/assets'));
-});
-
-gulp.task('vendors', () => {
-  const styles = gulp
-    .src('src/vendors/vendor.scss')
-    .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(
-      plugins.postcss([
-        require('postcss-import'),
-        require('postcss-copy')({ dest: 'dist/assets' }),
-        autoprefixer(),
-        cssnano(),
-      ])
-    )
-    .pipe(gulp.dest('dist/assets'));
-
-  const scriptSource = JSON.parse(fs.readFileSync('src/vendors/vendor.json', 'utf-8')).map(
-    (path) => `src/vendors/${path}`
-  );
-
-  const scripts = gulp
-    .src(scriptSource)
-    .pipe(plugins.concat('vendor.js'))
-    .pipe(plugins.babel({ presets: ['@babel/env'] }))
-    .pipe(plugins.uglify())
-    .pipe(gulp.dest('dist/assets'));
-
-  return merge(styles, scripts);
-});
-
-gulp.task('config', () => {
-  return gulp.src('src/config/*.json').pipe(gulp.dest('dist/config'));
-});
-
-gulp.task('locales', () => {
-  return gulp.src('src/locales/*.json').pipe(gulp.dest('dist/locales'));
-});
-
-gulp.task('assets', () => {
-  return gulp.src('src/assets/*.*').pipe(gulp.dest('dist/assets'));
-});
-gulp.task('clean', () => {
-  return gulp.src('dist/*').pipe(plugins.clean({ force: true }));
-});
 
 gulp.task('folders', () => {
   return gulp
@@ -171,26 +27,39 @@ gulp.task('folders', () => {
     .pipe(gulp.dest('dist'));
 });
 
-const buildAssets = gulp.parallel('assets');
-const buildVendors = gulp.parallel('vendors');
-const buildSettings = gulp.parallel('config', 'locales');
-const buildLiquid = gulp.parallel('templates', 'styles', 'scripts');
+gulp.task('liquid', () => {
+  return gulp.src('src/**/*.liquid').pipe(gulp.dest('dist'));
+});
 
-gulp.task(
-  'build',
-  gulp.series('clean', 'folders', buildAssets, buildSettings, buildVendors, buildLiquid)
-);
+gulp.task('bundle', () => {
+  return new Bundler(entryFile, { outDir, watch: false, minify: isProd }).bundle();
+});
+
+gulp.task('assets', () => {
+  return gulp.src('src/assets/*.*').pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('clean', () => {
+  return gulp.src(['dist/*', '.cache/*']).pipe(plugins.clean({ force: true }));
+});
+
+gulp.task('config', () => {
+  return gulp.src('src/config/*.json').pipe(gulp.dest('dist/config'));
+});
+
+gulp.task('locales', () => {
+  return gulp.src('src/locales/*.json').pipe(gulp.dest('dist/locales'));
+});
 
 gulp.task('watch', () => {
-  gulp.watch('src/assets/*.*', buildAssets);
-  gulp.watch('src/vendors/**/*.*', buildVendors);
-  gulp.watch([...sources, 'src/global/**/*.*'], buildLiquid);
-  gulp.watch(['src/config/*.json', 'src/locales/*.json'], buildSettings);
+  new Bundler(entryFile, { outDir }).bundle();
+
+  gulp.watch('src/**/*.liquid', gulp.parallel(['liquid']));
 
   themeKit.command('watch', {
     allowLive: true,
     env: 'development',
-    notify: path.join(__dirname, '.updated'),
+    notify: path.join(__dirname, '.cache/updated'),
   });
 });
 
@@ -199,13 +68,13 @@ gulp.task('serve', () => {
     port: 8080,
     open: false,
     notify: false,
-    reloadDelay: 1500,
+    reloadDelay: 1000,
     proxy: `https://${config.store}/`,
-    files: path.join(__dirname, '.updated'),
+    files: path.join(__dirname, '.cache/updated'),
     snippetOptions: {
       rule: {
         match: /<\/body>/i,
-        fn: function (snippet, match) {
+        fn: (snippet, match) => {
           return snippet + match;
         },
       },
@@ -213,4 +82,9 @@ gulp.task('serve', () => {
   });
 });
 
-gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
+const prepare = gulp.series('clean', 'folders');
+const copy = gulp.parallel('assets', 'config', 'locales', 'liquid');
+
+gulp.task('build', gulp.series(prepare, gulp.parallel(copy, 'bundle')));
+
+gulp.task('dev', gulp.series(prepare, copy, gulp.parallel('watch', 'serve')));
