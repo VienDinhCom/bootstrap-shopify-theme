61e74ea49ab2f1e62862a9764bb8a941226a57f7
Vien Dinh <maxvien@gmail.com>
2021-05-21 17:12:15 +0700

    Disable Inappropriate Options

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,64 +1,107 @@
+import uniq from 'lodash/uniq';
 import { ProductForm } from '@shopify/theme-product-form';
 
 (async () => {
   const templateElement = document.querySelector('.template-product');
 
-  if (templateElement) {
-    const formElement = templateElement.querySelector('.template-product__form');
-    const productHandle = formElement.dataset.productHandle;
-    const productData = await fetch(`/products/${productHandle}.js`).then((res) => res.json());
+  if (templateElement === null) return;
 
-    const optionValues = [];
+  const selectedValues = [];
 
-    function run() {
-      const formVariantElements = formElement.querySelectorAll('.template-product__form-variant');
+  const formElement = templateElement.querySelector('.template-product__form');
+  const productHandle = formElement.dataset.productHandle;
+  const productData = await fetch(`/products/${productHandle}.js`).then((res) => res.json());
+  const buttonElement = formElement.querySelector('button[type=submit]');
 
-      formVariantElements.forEach((currentVariantElement, currentVariantIndex) => {
-        const currentVariantValue = currentVariantElement.querySelector('select').value;
-        const nextVariantElement = formVariantElements[currentVariantIndex + 1];
+  buttonElement.disabled = true;
 
-        if (nextVariantElement) {
-          const nextVariantSelectElement = nextVariantElement.querySelector('select');
-          const nextVariantOptionElements = nextVariantElement.querySelectorAll('option');
+  function proceeding() {
+    const selectElements = formElement.querySelectorAll('.template-product__form-option select');
 
-          if (currentVariantValue !== optionValues[currentVariantIndex]) {
-            nextVariantSelectElement.value = '';
-          }
+    // Select Variants by Steps
+    selectElements.forEach((formSelectElement, index) => {
+      const currentSelectValue = formSelectElement.value;
+      const nextSelectElement = selectElements[index + 1];
 
-          if (currentVariantValue === '') {
-            nextVariantSelectElement.disabled = true;
-          } else {
-            nextVariantSelectElement.disabled = false;
-          }
+      if (nextSelectElement) {
+        if (currentSelectValue !== selectedValues[index]) {
+          nextSelectElement.value = '';
+        }
 
-          optionValues[currentVariantIndex] = currentVariantValue;
+        if (currentSelectValue === '') {
+          nextSelectElement.disabled = true;
+        } else {
+          nextSelectElement.disabled = false;
+        }
+
+        selectedValues[index] = currentSelectValue;
+      }
+    });
+
+    // Disable Inappropriate Options
+    const selectOneElement = selectElements[0];
+    const selectTwoElement = selectElements[1];
+    const selectThreeElement = selectElements[2];
+
+    (() => {
+      const okayOptions = uniq(
+        productData.variants
+          .filter(({ option1 }) => option1 === selectOneElement.value)
+          .map(({ option2 }) => option2)
+      );
+
+      const selectTwoOptionElements = selectTwoElement.querySelectorAll('option');
+
+      selectTwoOptionElements.forEach((selectTwoOptionElement) => {
+        if (okayOptions.includes(selectTwoOptionElement.value)) {
+          selectTwoOptionElement.disabled = false;
+        } else {
+          selectTwoOptionElement.disabled = true;
         }
       });
-    }
+    })();
 
-    run();
+    (() => {
+      const okayOptions = uniq(
+        productData.variants
+          .filter(
+            ({ option1, option2 }) =>
+              option1 === selectOneElement.value && option2 === selectTwoElement.value
+          )
+          .map(({ option3 }) => option3)
+      );
 
-    const productForm = new ProductForm(formElement, productData, {
-      onOptionChange: (event) => {
-        const variant = event.dataset.variant;
+      const selectThreeOptionElements = selectThreeElement.querySelectorAll('option');
 
-        run();
-
-        // if (variant === null) {
-        //   // The combination of selected options does not have a matching variant
-        //   alert('Does not have a matching variant');
-        // } else if (variant && !variant.available) {
-        //   // The combination of selected options has a matching variant but it is
-        //   // currently unavailable
-
-        //   alert('Unavailable');
-        // } else if (variant && variant.available) {
-        //   // The combination of selected options has a matching variant and it is
-        //   // available
-
-        //   alert('Available');
-        // }
-      },
-    });
+      selectThreeOptionElements.forEach((selectThreeOptionElement) => {
+        if (okayOptions.includes(selectThreeOptionElement.value)) {
+          selectThreeOptionElement.disabled = false;
+        } else {
+          selectThreeOptionElement.disabled = true;
+        }
+      });
+    })();
   }
+
+  proceeding();
+
+  new ProductForm(formElement, productData, {
+    onOptionChange: (event) => {
+      const variant = event.dataset.variant;
+
+      proceeding();
+
+      if (variant === null) {
+        // The combination of selected options does not have a matching variant
+      } else if (variant && !variant.available) {
+        // The combination of selected options has a matching variant but it is
+        // currently unavailable
+      } else if (variant && variant.available) {
+        // The combination of selected options has a matching variant and it is
+        // available
+
+        buttonElement.disabled = false;
+      }
+    },
+  });
 })();
