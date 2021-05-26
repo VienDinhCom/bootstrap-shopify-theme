7e9f73080809012f628d50671cdc7f544621900c
Vien Dinh <maxvien@gmail.com>
2021-05-26 10:36:53 +0700

    Hide Preview Bar

--- gulpfile.js
+++ gulpfile.js
@@ -91,7 +91,9 @@ gulp.task('serve', () => {
     snippetOptions: {
       rule: {
         match: /<\/body>/i,
-        fn: (snippet, match) => snippet + match,
+        fn: (snippet, match) => {
+          return snippet + '<style>iframe#preview-bar-iframe { display: none; }</style>' + match;
+        },
       },
     },
   });
