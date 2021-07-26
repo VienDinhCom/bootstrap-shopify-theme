7afc007c0f24a2b038d60ba0f6221d80726adc85
Vien Dinh <maxvien@gmail.com>
2021-07-26 12:12:44 +0700

    migrate to new css modules

--- src/main.js
+++ src/main.js
@@ -1,12 +1,64 @@
+/* Global
+----------------------------------------------------*/
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import 'smartmenus';
 import 'smartmenus-bootstrap-4';
+import 'smartmenus-bootstrap-4/jquery.smartmenus.bootstrap-4.css';
 
-import './main.scss';
+import 'swiper/swiper-bundle.css';
 
+/* Layout
+----------------------------------------------------*/
+import './layout/theme.scss';
+
+import './layout/password.scss';
+
+/* Snippets
+----------------------------------------------------*/
 import './snippets/address-form';
+import './snippets/address-form.scss';
+
+import './snippets/article-item.scss';
+import './snippets/collection-item.scss';
+
 import './snippets/featured-slider';
+import './snippets/featured-slider.scss';
+
+import './snippets/footer.scss';
+
+import './snippets/page-header.scss';
+
+import './snippets/product-item.scss';
+
+/* Sections
+----------------------------------------------------*/
+import './sections/slideshow.scss';
+
+/* Templates
+----------------------------------------------------*/
+import './templates/customers/addresses.scss';
 
 import './templates/customers/login';
+
+import './templates/customers/login.scss';
+
+import './templates/article.scss';
+
+import './templates/blog.scss';
+
+import './templates/collection.scss';
+
+import './templates/gift_card.scss';
+
+import './templates/list-collections.scss';
+
+import './templates/password.scss';
+
 import './templates/product';
+import './templates/product.scss';
+
 import './templates/search';
+
+import './templates/search.scss';
